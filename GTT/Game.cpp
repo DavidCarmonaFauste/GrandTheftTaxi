ef7b64befcc55f068e49f1afc05b30f5681e91f1
JESUS RAMOS <jeramauni@gmail.com>
2019-02-11 16:02:51 +0100

    Animation system checked

--- GTT/Game.cpp
+++ GTT/Game.cpp
@@ -10,13 +10,6 @@ SDL_Renderer* Game::renderer = nullptr;
 Game::Game(SDL_Window *window_, SDL_Renderer *renderer_) {
 	Game::window = window_;
 	Game::renderer = renderer_;
-
-
-
-	// FOR TESTING
-	test = new Sprite();
-	test->loadAnimation("./../Assets/sprites/test_animation.png", "test", 8, 2);
-	test->playAnimation("test", 10);
 }
 
 Game::~Game() {
@@ -24,8 +17,6 @@ Game::~Game() {
 }
 
 bool Game::update(double deltaTime) {
-	test->update(deltaTime);
-
 	handleEvents();
 
 	return exit;
@@ -36,9 +27,6 @@ void Game::handleEvents() {
 
 	while (SDL_PollEvent(&event) && !exit) {
 		if (event.type == SDL_QUIT) exit = true;
-		else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_p) test->pauseAnimation();
-		else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_s) test->stopAnimation();
-		else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_r) test->resumeAnimation();
-		else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_y) test->playAnimation("test", 10);
+	
 	}
 }
