aa970bcfa6fb67bb7ad2c2e4b1bf7b7c1f2d1f99
dagil02 <dagil02@ucm.es>
2019-03-26 17:01:11 +0100

    nueva rama + base enemigo

---
+++ GTT/EnemyVehicle.cpp
@@ -0,0 +1,12 @@
+#include "EnemyVehicle.h"
+
+
+
+EnemyVehicle::EnemyVehicle(int x, int y, Resources::VehicleId id)
+{
+}
+
+
+EnemyVehicle::~EnemyVehicle()
+{
+}
---
+++ GTT/EnemyVehicle.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "Container.h"
+#include "PhysicObject.h"
+#include <Box2D/Box2D.h>
+#include "Animation.h"
+#include "ControlType.h"
+#include "Resources.h"
+#include <vector>
+#include "Health.h"
+#include <cmath> 
+#include "Turret.h"
+class EnemyVehicle :
+	public Container, public Observable
+{
+public:
+	EnemyVehicle(int x, int y, Resources::VehicleId id);
+	~EnemyVehicle();
+
+	//Get
+	PhysicObject* GetPhyO();
+	float32 GetMaxSpeed();
+	float32 GetMaxBackwardSpeed();
+	float32 GetTurnSpeed();
+	float32 GetAcceleration();
+
+	Health* getHealthComponent();
+
+	
+	virtual void EquipTurret(Turret* turret);
+	Turret* getCurrentTurret();
+	virtual void setPosition(const Vector2D &pos, bool force = false) override;
+	virtual void handleInput(Uint32 time, const SDL_Event& event);
+	virtual void render(Uint32 time);
+	virtual void update(Uint32 time);
+
+protected:
+	float32 maxSpeed_;
+	float32 turnSpeed_;
+	float32 acceleration_;
+
+	ControlType* control_;
+
+
+
+	PhysicObject* phyO_;
+	Animation* sprite_;
+	AimComponent* aimC_;//forma de apuntar con la torreta (depende de si es Jugador o IA)
+	Health* health_;
+};
+
