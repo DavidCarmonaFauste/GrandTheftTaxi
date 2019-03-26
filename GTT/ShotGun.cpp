cc5140644cfba7abef3d38d4421bc707d31dfb7e
BraisMorgade <36987428+BraisMorgade@users.noreply.github.com>
2019-03-26 17:38:14 +0100

    ShotGunChargedShot

--- GTT/ShotGun.cpp
+++ GTT/ShotGun.cpp
@@ -11,7 +11,9 @@ ShotGun::ShotGun()
 	reloadTime_ = 4000;
 	perfRelIni_ = 0.6;
 	perfRelSeg_ = 0.2;
+	chargeTime_ = 2000;
 	normalB = SHOTGUNB;
+	specialB = GUNB;
 	magazine_ = new stack<double> [maxAmmo_];
 	for (int i = 0; i < maxAmmo_; i++) {
 		magazine_->push(1.0);
@@ -28,5 +30,27 @@ ShotGun::ShotGun()
 	animC_->loadAnimation(path_, "default");
 	animC_->playAnimation("default");
 }
+
+void ShotGun::Shoot()
+{
+		if (!magazine_->empty() && !reloading_) {
+			if (SDL_GetTicks() - lastTimeShot_ >= cadence_) {
+				if (SDL_GetTicks() - chargeprogress_ >= chargeTime_) {
+					shC_->changeProperties(dispersionangle_ * 2, numPellets_ * 2);
+					shC_->shoot(specialB);
+				}
+				else {
+					shC_->changeProperties(dispersionangle_, numPellets_);
+					shC_->shoot(normalB);
+				}
+				magazine_->pop();
+				lastTimeShot_ = SDL_GetTicks();
+				animC_->playAnimation("idle", 3.5f, false);
+				chargeprogress_ = SDL_GetTicks();
+			}
+		}
+}
+
+
 
 
--- GTT/StaticSC.h
+++ GTT/StaticSC.h
@@ -6,6 +6,7 @@ class StaticSC : public ShootComponent
 public:
 	StaticSC(Turret* turret) :ShootComponent(turret) {};
 	virtual void shoot(ProyectileType prType) {};
+	virtual void changeProperties(double prop1, int prop2) {};
 	virtual ~StaticSC() {};
 };
 
