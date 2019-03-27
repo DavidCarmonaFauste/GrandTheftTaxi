202a29bcae73ffd252c6919c36f37f2d60d6eab6
BraisMorgade <36987428+BraisMorgade@users.noreply.github.com>
2019-03-27 16:04:39 +0100

    AutomaticWeapons

--- GTT/MachineGun.cpp
+++ GTT/MachineGun.cpp
@@ -2,6 +2,7 @@
 #include "LinearSC.h"
 #include "Animation.h"
 #include "Reticule.h"
+#include "ShootIC.h"
 
 
 MachineGun::MachineGun()
@@ -9,6 +10,12 @@ MachineGun::MachineGun()
 	maxAmmo_ = 25;
 	cadence_ = 50;
 	reloadTime_ = 3000;
+	perfRelIni_ = 0.6;
+	perfRelSeg_ = 0.2;
+	chargeTime_ = 2000;
+	normalB = GUNB;
+	specialB = SHOTGUNB;
+	automatic_ = true;
 	magazine_ = new stack<double>[maxAmmo_];
 	for (int i = 0; i < maxAmmo_; i++) {
 		magazine_->push(1.0);
@@ -18,8 +25,10 @@ MachineGun::MachineGun()
 	shC_ = new LinearSC(this);
 	height_ = 200;
 	width_ = 100;
+	animC_->loadAnimation(path_, "default");
 	animC_->loadAnimation(path_, "idle");
-	animC_->playAnimation("idle");
+	animC_->playAnimation("default");
 	
 }
+
 
