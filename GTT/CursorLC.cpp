86df37b443b9dacd19ad624579503d8baecbbb5b
Pablo Martín Navarro <pablma13@ucm.es>
2019-04-01 18:24:53 +0200

    Init

--- GTT/CursorLC.cpp
+++ GTT/CursorLC.cpp
@@ -1,7 +1,10 @@
 #include "CursorLC.h"
 #include "Game.h"
+#include <iostream>
+#include <list>
 
-
+bool init = false;
+bool creating = false;
 CursorLC::CursorLC()
 {
 }
@@ -10,10 +13,28 @@ void CursorLC::update(GameObject * o, Uint32 deltaTime)
 {
 	int x, y;
 	SDL_GetMouseState(&x, &y);
+	if(!init)
+	{
+		init = true;
+		cout << "init -> ";
+		std::list<int> open;
+		std::list<int> close;
+		cout << "list created -> ";
+		if (!creating)
+		{
+			creating = true;
+			cout << "creting ->";
+			
+		}
+	}
 	o->setPosition(Vector2D(((double)x/(double)Game::getInstance()->getWindowWidth())*(double)Game::getInstance()->getCameraWidth() - o->getWidth()/2,
 		((double)y / (double)Game::getInstance()->getWindowHeight())*(double)Game::getInstance()->getCameraHeight()- o->getHeight()/2));
 }
 
+void push(std::list<int> l)
+{
+
+}
 
 CursorLC::~CursorLC()
 {
