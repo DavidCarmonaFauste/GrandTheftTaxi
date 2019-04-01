f0e483aa8fecd2c511a0b9ceb8594814872a5f2f
Pablo Martín Navarro <pablma13@ucm.es>
2019-04-01 18:51:45 +0200

    save 1

--- GTT/CursorLC.cpp
+++ GTT/CursorLC.cpp
@@ -1,7 +1,7 @@
 #include "CursorLC.h"
 #include "Game.h"
 #include <iostream>
-#include <list>
+
 
 bool init = false;
 bool creating = false;
@@ -16,24 +16,26 @@ void CursorLC::update(GameObject * o, Uint32 deltaTime)
 	if(!init)
 	{
 		init = true;
-		cout << "init -> ";
-		std::list<int> open;
-		std::list<int> close;
-		cout << "list created -> ";
+		cout << "init..." ;
+		std::list<Vector2D> open;
+		std::list<Vector2D> close;
+		cout << "list created..." ;
 		if (!creating)
 		{
 			creating = true;
-			cout << "creting ->";
-			
+			cout << "creting..." + '\n';
+			push(open, Vector2D(0, 0), "open list");
+			push(close, Vector2D(0, 0), "close list");
 		}
 	}
 	o->setPosition(Vector2D(((double)x/(double)Game::getInstance()->getWindowWidth())*(double)Game::getInstance()->getCameraWidth() - o->getWidth()/2,
 		((double)y / (double)Game::getInstance()->getWindowHeight())*(double)Game::getInstance()->getCameraHeight()- o->getHeight()/2));
 }
 
-void push(std::list<int> l)
+void CursorLC::push(std::list<Vector2D> l, Vector2D v, string t)
 {
-
+	l.push_back(v);
+	cout << "pushed x=" + std::to_string(v.x) + " y=" + std::to_string(v.y) + " at " + t + '\n';
 }
 
 CursorLC::~CursorLC()
--- GTT/CursorLC.h
+++ GTT/CursorLC.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "LogicComponent.h"
+#include <list>
+#include <vector>
 
 class CursorLC : public LogicComponent {
 public:
 	CursorLC();
 	virtual void update(GameObject* o, Uint32 deltaTime);
+	void push(std::list<Vector2D> l, Vector2D v, string t);
 	virtual ~CursorLC();
 };
 
