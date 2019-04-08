0eaf43e3858bea97ebcb0cf284a1b9a5ff3f4ddf
Jesús Ramos <jeramauni@gmail.com>
2019-04-08 18:37:08 +0200

    Sounds 0.1

--- GTT/TaxiSoundManagerCP.h
+++ GTT/TaxiSoundManagerCP.h
@@ -1,18 +1,21 @@
 #pragma once
 #include "LogicComponent.h"
 #include "SoundManager.h"
+#include "Observer.h"
 
 class Vehicle;
 
 class TaxiSoundManagerCP :
-	public LogicComponent
+	public LogicComponent, public Observer
 {
 public:
-	TaxiSoundManagerCP();
-	void Start();
+	TaxiSoundManagerCP(Vehicle* v);
 	virtual void update(GameObject* o, Uint32 deltaTime);
+	virtual bool receiveEvent(Event& e);
 	virtual ~TaxiSoundManagerCP();
 private:
 	SoundManager* s_;
+	Vehicle* v_;
+	int channel_;
 };
 
