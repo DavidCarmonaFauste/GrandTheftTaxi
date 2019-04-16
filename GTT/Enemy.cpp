44ab0f6d85c45c4f0e897f5135b5f7d7b1047f05
BraisMorgade <36987428+BraisMorgade@users.noreply.github.com>
2019-04-16 17:29:55 +0200

    PatrolTest

--- GTT/Enemy.cpp
+++ GTT/Enemy.cpp
@@ -1,8 +1,8 @@
 #include "Enemy.h"
+#include "Reticule.h"
 
 
-
-Enemy::Enemy(int x, int y, VehicleInfo r, KeysScheme k) :Car(x, y) {
+Enemy::Enemy(int x, int y, VehicleInfo r) :Car(x, y) {
 	this->setWidth(r.width);
 	this->setHeight(r.height);
 
@@ -16,28 +16,18 @@ Enemy::Enemy(int x, int y, VehicleInfo r, KeysScheme k) :Car(x, y) {
 
 	// Health
 	health_ = new Health(ENEMY_HP);
-	//health_->setDamageOverTime(DMG_OVER_TIME, DMG_FREQUENCY);
 	addLogicComponent(health_);
 
-	/*shIC_ = new ShootIC();
-	reIC_ = new ReloadInputComponent();
-	aimC_ = new AimAtCursorAC();
-	addInputComponent(new ChangeWeaponIC());
-
-	for (int i = 0; i < MAXTURRETS; i++) {
-		turrets_[i] = nullptr;
-	}*/
-
-	 //Movement
-		
+	//Movement
+	speed_ = 5;
 	//IA
 	
 
 	// Physics
 	phyO_ = new PhysicObject(b2_dynamicBody, width_, height_, position_.x, position_.y);
 	phyO_->getBody()->SetUserData(this);
-	phyO_->getBody()->SetLinearDamping(2.0f);
-	phyO_->getBody()->SetAngularDamping(2.0f);
+	//phyO_->getBody()->SetLinearDamping(2.0f);
+	//phyO_->getBody()->SetAngularDamping(2.0f);
 	addLogicComponent(phyO_);
 }
 
@@ -54,14 +44,75 @@ void Enemy::Die()
 
 void Enemy::update(Uint32 deltaTime)
 {
-	if (bodyReadyToDestroy_) {
-		delLogicComponent(phyO_);
-		delete phyO_;
-		phyO_ = nullptr;
-		setActive(false);
+	if (active_) {
+		if (destinated_) {
+			if (direction_.x < 0) {
+				if (getCenter().x < destination_.x) {
+					phyO_->getBody()->SetLinearVelocity(Vector2D(0, 0));
+					destinated_ = false;
+					return;
+				}
+			}
+			else if (direction_.x > 0) {
+				if (getCenter().x > destination_.x) {
+					phyO_->getBody()->SetLinearVelocity(Vector2D(0, 0));
+					destinated_ = false;
+					return;
+				}
+			}
+			else if (direction_.y < 0) {
+				if (getCenter().y < destination_.y) {
+					phyO_->getBody()->SetLinearVelocity(Vector2D(0, 0));
+					destinated_ = false;
+					return;
+				}
+			}
+			else if (direction_.y > 0) {
+				if (getCenter().y > destination_.y) {
+					phyO_->getBody()->SetLinearVelocity(Vector2D(0, 0));
+					destinated_ = false;
+					return;
+				}
+			}
+			direction_ = Vector2D(destination_.x - getCenter().x, destination_.y - getCenter().y);
+			direction_.Normalize();
+			float angle = atan2f(-direction_.x, direction_.y);
+			angle += 90.0 / 180.0*M_PI;
+			phyO_->getBody()->SetTransform(phyO_->getBody()->GetPosition(), angle);
+			phyO_->getBody()->SetLinearVelocity(Vector2D(direction_.x * speed_, direction_.y * speed_));
+		}
+		else {
+			destinated_ = true;
+			destination_ = route[node++%4];
+			direction_ = Vector2D(destination_.x - getCenter().x, destination_.y - getCenter().y);
+			direction_.Normalize();
+		}
+		if (bodyReadyToDestroy_) {
+			delLogicComponent(phyO_);
+			delete phyO_;
+			phyO_ = nullptr;
+			setActive(false);
+		}
+		Car::update(deltaTime);
 	}
-	Car::update(deltaTime);
 }
+
+void Enemy::handleInput(Uint32 deltaTime, const SDL_Event & event)
+{
+	if (active_) {
+		if (event.type == SDL_MOUSEBUTTONDOWN) {
+			if (event.button.button == SDL_BUTTON_RIGHT) {
+				destinated_ = true;
+				destination_ = Vector2D(Reticule::GetInstance()->getCenter().x + Game::getInstance()->getCamera(GAME_CAMERA)->getPosition().x,
+					Reticule::GetInstance()->getCenter().y + Game::getInstance()->getCamera(GAME_CAMERA)->getPosition().y);
+				direction_ = Vector2D(destination_.x - getCenter().x, destination_.y - getCenter().y);
+				direction_.Normalize();
+			}
+		}
+		Car::handleInput(deltaTime, event);
+	}
+}
+
 
 Enemy::~Enemy()
 {
--- GTT/Enemy.h
+++ GTT/Enemy.h
@@ -5,12 +5,20 @@
 class Enemy :public Car
 {
 public:
-	Enemy(int x, int y, VehicleInfo r, KeysScheme k);
+	Enemy(int x, int y, VehicleInfo r);
 	virtual void Damage(double damage);
 	virtual void Die();
 	virtual void update(Uint32 deltaTime);
+	virtual void handleInput(Uint32 deltaTime, const SDL_Event& event);
+
 	virtual ~Enemy();
 private:
 	bool bodyReadyToDestroy_;
+	bool destinated_;
+	Vector2D destination_;
+	Vector2D direction_;
+	Vector2D route[4] = {Vector2D(0,0), Vector2D(400,0), Vector2D(400, 400), Vector2D(0, 400)};
+	int node = -1;
+	int speed_;
 };
 
