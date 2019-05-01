ed2367b4797266f16159815ce6332fa6eef2276a
Jose <jossan10@ucm.es>
2019-05-01 19:33:39 +0200

    Rework ClickMouseIC
    Se establece la clase para que tratar únicamente el container tipo Button en su método HandleInput
    
    Se añade un atributo privado  del tipo Button y un método setButton(Button b); que es llamado cuando se construye un nuevo componente MouseClickIC en la constructora de la clase Button.

--- GTT/MouseClickIC.h
+++ GTT/MouseClickIC.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "ControlType.h"
 
+
+class Button; 
+
 class MouseClickIC :
 	public ControlType {
 
@@ -8,11 +11,16 @@ public:
 	MouseClickIC(int key = SDL_BUTTON_LEFT);
 	virtual ~MouseClickIC ();
 
+	//redefnie el método dado que esta clase sólo usa Button para tratar su input
 	virtual void handleInput(GameObject* o, Uint32 deltaTime, const SDL_Event& event);
+
 	virtual void update(GameObject* o, Uint32 deltaTime){}
+
+	void setButton(Button* b);
 
 
 private:
+	Button* b_;
 	int mouseClickKey_;
 };
 
