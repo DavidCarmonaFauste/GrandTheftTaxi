becc2de781a06b1867da560af55ffbf66ef35a73
dagil02 <dagil02@ucm.es>
2019-05-02 12:10:56 +0200

    cambios phrases.h

--- GTT/Phrases.h
+++ GTT/Phrases.h
@@ -1,10 +1,10 @@
 #pragma once
 #include <vector>
-
+#include <string>
 
 using namespace std;
 
-const vector <pair<string, string>> disparo{
+ const vector <pair<string, string>> disparo = {
 	{"MUERE","DIE"},
 	{"EN LA CABEZA","ON THE HEAD"},
 	{"Puede que la próxima","Maybe next time"}
