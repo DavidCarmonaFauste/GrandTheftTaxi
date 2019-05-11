72d154e75935dd1607308191f6262ee42d05227d
BraisMorgade <36987428+BraisMorgade@users.noreply.github.com>
2019-05-11 17:12:02 +0200

    CompilationErrorFix

--- GTT/Phrases.h
+++ GTT/Phrases.h
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+
 const vector <pair<string, string>> disparo = {
    {"MUERE","DIE"},
    {"EN LA CABEZA","ON THE HEAD"},
@@ -12,8 +13,8 @@ const vector <pair<string, string>> disparo = {
 };
 
 const vector <pair<string, string>> muerte = {
-   {"¡¡NET NET NET NET NEEEEEEEEEEEET!!", "¡¡NET NET NET NET NEEEEEEEEEEEET!!"},
-   {"¡¿TOVARISHCHI?! ¡¿TOVARISHCHI?!","¡¿TOVARISHCHI?! ¡¿TOVARISHCHI?!"},
+   {"NET NET NET NET NEEEEEEEEEEEET!!", "NET NET NET NET NEEEEEEEEEEEET!!"},
+   {"TOVARISHCHI?! TOVARISHCHI?!","TOVARISHCHI?! TOVARISHCHI?!"},
    {"Hasta siempre camarada...","TAXI FOREVER"}
 
 };
@@ -25,25 +26,24 @@ const vector <pair<string, string>> fuego = {
 };
 
 const vector <pair<string, string>> kill = {
-  {"¡JAH! ¡Mira a esos pobres desgraciados, parecen palomitas! ¿da? ¡JA JA JA!","ONE LESS"},
+  {"JAH! Mira a esos pobres desgraciados, parecen palomitas! da? JA JA JA!","ONE LESS"},
   {"P**O VTC","F***ING VTC"},
-  {"¡istreblyayet! ¡istreblyayet! ¡istreblyayet!","¡istreblyayet! ¡istreblyayet! ¡istreblyayet!"}
+  {"istreblyayet! istreblyayet! istreblyayet!","istreblyayet! istreblyayet! istreblyayet!"}
 
 };
 
 const vector <pair<string, string>> tutorial = {
-	{" Para atacar solo tiene que pulsar el -click izquierdo- ¿da ?" , ""},
+	{" Para atacar solo tiene que pulsar el -click izquierdo- da ?" , ""},
 	{" Atiende tovarishchi, usa -WASD- para conducir" , ""},
-	{" Recuerda camarada si los giros se te hacen algo tyazhelyy, usa -SPACEBAR- para derrapar ¿da ?" , ""},
-	{" Si te quedas sin gasolina puedes usar las gasolineras para repostar ¿da ? Simplemente acercate y pulsa -G- ¿da ?" , ""},
-	{" ¡Ahí están esos sucios VTC! hazle el favor a Galenka de destruirlos ¿da ?" , ""}
+	{" Recuerda camarada si los giros se te hacen algo tyazhelyy, usa -SPACEBAR- para derrapar da ?" , ""},
+	{" Si te quedas sin gasolina puedes usar las gasolineras para repostar da ? Simplemente acercate y pulsa -G- da ?" , ""},
+	{" Ahi estan esos sucios VTC! hazle el favor a Galenka de destruirlos da ?" , ""}
 };
 
 const vector <pair<string, string>> pocaVida = {
-	{"¡Por Lenin camarada! te están haciendo sopa de svekla" , ""},
-	{"¡¡POR LA MADRE RUSIA!! ¡BYSTRO BYSTRO VETE A UNA GASOLINERA CUANTO ANTES!" , ""},
-}
-
+	{"Por Lenin camarada! te estan haciendo sopa de svekla" , ""},
+	{"POR LA MADRE RUSIA!! BYSTRO BYSTRO VETE A UNA GASOLINERA CUANTO ANTES!" , ""}
+};
 
 
 
