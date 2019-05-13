a3e25267c8155d7b2a99779e6815a747be574614
Sergio Abreu García <saggamedeveloper@gmail.com>
2019-05-13 20:05:32 +0200

    VisualLeakDetector disabled

--- GTT/main.cpp
+++ GTT/main.cpp
@@ -1,6 +1,9 @@
 #pragma once
 
-#include <vld.h>
+// Visual Leak Detector library:
+// uncomment the include to check for memory leaks
+//#include <vld.h>
+
 #include "SDL.h"
 #include "SDL_image.h"
 
@@ -13,7 +16,6 @@ using namespace std;
 typedef unsigned int uint;
 
 int main(int argc, char* argv[]) {
-
 	Game::getInstance()->initInstance(); //initInstance() only once
 	Game::getInstance()->run();
 	Game::destroyInstance();
