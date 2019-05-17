85d463d9751888a23f5b50f92a17c0a8ba19c28f
Sergio Abreu García <saggamedeveloper@gmail.com>
2019-05-18 00:26:24 +0200

    Installer without menu

--- GTT/GameStateMachine.cpp
+++ GTT/GameStateMachine.cpp
@@ -53,7 +53,7 @@ void GameStateMachine::initStates() {
 		
 	//primer estado al ejectuar app
 	//setState(NAME_MAINMENU_STATE);
-	setState(NAME_MAINMENU_STATE);
+	setState(NAME_MAIN_STATE);
 }
 
 void GameStateMachine::fromMainStateToGasMainMenu () {
